0d921a98ee46f371ffceb42a7fc19e615046fbfa
IsaacToluwani <toluwani.isaac@gmail.com>
2024-11-18 03:58:16 +0100

    task_0 completed

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,12 @@
+import $ from "jquery";
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+/*
+const p1 = document.createElement("p");
+p1.textContent("Holberton Dashboard")
+document.body.append(p1)
+
+*/
